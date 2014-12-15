9bca0a804edf12a5a2e5f2fa8e44f196f002ff03
mgi166 <skskoari@gmail.com>
2014-12-15 23:51:28 +0900

    * set title, and log

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('title', 'React Othello');
+
 app.get('/', function (req, res) {
     res.send('hello');
 });
 
 app.listen(3000);
+
+console.log('Server started: http://localhost:3000');
